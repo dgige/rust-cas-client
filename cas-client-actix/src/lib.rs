d8b31afd3be24fc0f616dd47e99700197e9d5a61
Julien Marignale <julien.marignale@u-picardie.fr>
2020-07-25 22:54:18 +0200

    Enhanced configurability and tests
    (kljensen/rust-cas-client/commit/dc96be)

--- cas-client-actix/src/lib.rs
+++ cas-client-actix/src/lib.rs
@@ -28,6 +28,9 @@ const AFTER_LOGGED_IN_URL_SESSION_KEY: &str = "after_logged_in_url";
 #[derive(Clone)]
 pub struct ActixCasClient {
     cas_client: CasClient,
+    // Indicates that the server's URL should be
+    // used as the CAS `service_url`.
+    server_is_service: bool,
 }
 
 fn ticket_for_query_string(
@@ -40,7 +43,15 @@ fn ticket_for_query_string(
 
 impl ActixCasClient {
     pub fn new(cas_client: CasClient) -> Self {
-        ActixCasClient { cas_client }
+        ActixCasClient {
+            cas_client,
+            server_is_service: false,
+        }
+    }
+
+    pub fn set_server_is_service(&mut self, server_is_service: bool) -> &mut Self {
+        self.server_is_service = server_is_service;
+        self
     }
 
     pub fn login_url(&self) -> String {
@@ -72,17 +83,30 @@ where
         ok(ActixCasClientMiddleware {
             service,
             cas_client: self.cas_client.clone(),
+            server_is_service: self.server_is_service,
         })
     }
 }
 pub struct ActixCasClientMiddleware<S> {
     service: S,
     cas_client: CasClient,
+    server_is_service: bool,
 }
 
 #[derive(Debug, Deserialize)]
 struct CasTicketQuery {
     ticket: String,
+}
+
+fn host_scheme_for_request(req: &ServiceRequest) -> String {
+    let connection_info = req.connection_info();
+    let the_url = format!("{}://{}", connection_info.scheme(), connection_info.host(),);
+    the_url
+}
+
+fn url_for_request(req: &ServiceRequest) -> String {
+    let the_url = format!("{}{}", host_scheme_for_request(req), req.uri());
+    the_url
 }
 
 impl<S, B> ActixCasClientMiddleware<S>
@@ -114,14 +138,20 @@ where
     pub(self) fn authenticate_user(&self, req: &mut ServiceRequest) -> Option<HttpResponse> {
         let ticket = ticket_for_query_string(req.query_string());
         match ticket {
-            Ok(Some(ticket)) => self.handle_ticket(req, ticket),
-            _ => self.handle_needs_authentication(req),
+            Ok(Some(ticket)) => {
+                info!("Ticket = {}!", ticket);
+                self.handle_ticket(req, ticket)
+            }
+            _ => {
+                info!("Ticket not found!");
+                self.handle_needs_authentication(req)
+            }
         }
     }
 
     pub(self) fn authenticated_or_error(&self, req: &mut ServiceRequest, status_code: http::StatusCode) -> Option<HttpResponse> {
         let session = req.get_session();
-        if let Ok(Some(_)) = session.get::<CasUser>(CAS_USER_SESSION_KEY) {
+        if let Ok(None) = session.get::<CasUser>(CAS_USER_SESSION_KEY) {
             return Some(HttpResponse::build(status_code).finish())
         }
         None
@@ -129,7 +159,13 @@ where
 
     fn handle_needs_authentication(&self, req: &mut ServiceRequest) -> Option<HttpResponse> {
         self.set_after_logged_in_url(req);
-        let response = match self.cas_client.login_url() {
+        let login_url = match self.server_is_service {
+            true => self
+                .cas_client
+                .login_url_for_service(&host_scheme_for_request(req)),
+            false => self.cas_client.login_url(),
+        };
+        let response = match login_url {
             Some(login_url) => HttpResponse::build(http::StatusCode::TEMPORARY_REDIRECT)
                 .header(http::header::LOCATION, login_url)
                 .finish(),
@@ -168,13 +204,7 @@ where
 
     pub(self) fn set_after_logged_in_url(&self, req: &mut ServiceRequest) {
         let session = req.get_session();
-        let connection_info = req.connection_info();
-        let after_logged_in_url = format!(
-            "{}://{}{}",
-            connection_info.scheme(),
-            connection_info.host(),
-            req.uri()
-        );
+        let after_logged_in_url = url_for_request(req);
         let result = session.set(AFTER_LOGGED_IN_URL_SESSION_KEY, after_logged_in_url);
         if let Err(err) = result {
             error!(
@@ -225,62 +255,144 @@ mod cas_client_actix_test {
     use super::*;
     use actix_http::httpmessage::HttpMessage;
     use actix_session::CookieSession;
-    use actix_web::http::StatusCode;
     use actix_web::{
-        get, middleware,
+        // client::{Client, ClientResponse},
+        http::StatusCode,
+        middleware,
         test::{start, TestServer},
         App, HttpRequest,
     };
 
     const SESSION_COOKIE_NAME: &str = "foo";
+    const LOGIN_PATH: &str = "/login";
+    const LOGOUT_PATH: &str = "/logout";
+    const USER_PATH: &str = "/user";
+    const PROTECTED_PATH_403: &str = "/protected-403";
+    const PROTECTED_PATH_404: &str = "/protected-404";
+    const CAS_URL: &str = "http://fake.cas";
 
-    #[get("/")]
-    async fn guest(_req: HttpRequest) -> Result<HttpResponse, Error> {
+    fn get_cas_client(
+        auth_service: &str,
+        cas_url: &str,
+        behavior: NoAuthBehavior,
+    ) -> ActixCasClient {
+        let mut cas_client = CasClient::new(&cas_url).unwrap();
+        cas_client.set_no_auth_behavior(behavior);
+        cas_client.set_login_service(auth_service);
+        let mut a = ActixCasClient::new(cas_client);
+        a.set_server_is_service(true);
+        a
+    }
+
+    async fn guest() -> Result<HttpResponse, Error> {
         Ok(HttpResponse::build(StatusCode::OK)
             .content_type("text/html; charset=utf-8")
             .body("Welcome <b>Guest</b>!<br><a href='/user'>Login</a>"))
     }
 
-    #[get("/user")]
     async fn user(
         req: HttpRequest,
         cas_client: web::Data<ActixCasClient>,
     ) -> Result<HttpResponse, Error> {
         let session = req.get_session();
-        let user = session.get::<CasUser>("cas_user").unwrap().unwrap();
+        let user_session = session.get::<CasUser>("cas_user");
+        let user = user_session.unwrap_or(None);
+        let username = match user {
+            Some(user) => user.username().to_owned(),
+            None => "guest".to_owned(),
+        };
         Ok(HttpResponse::build(StatusCode::OK)
             .content_type("text/html; charset=utf-8")
             .body(format!(
                 "Welcome <b>{}</b>!<br><a href='{}'>Logout</a>",
-                user.username(),
+                username,
                 cas_client.logout_url()
             )))
     }
 
     fn get_server() -> TestServer {
+        // This will return a TestServer instance and you can
+        // get ClientResponse objects from it like
+        // `srv.get(url).send().await.unwrap()
+        // You could also construct your own client like this
+        //  let http_client = Client::default();
+        //  let req = http_client.get(srv.url(USER_PATH).as_str()).send();
+        //  let resp = req.get(url).send().await.unwrap()
+        // You'd do that, e.g. if you needed to get the
+        // servers full URL.
         let srv = start(|| {
-            let cas = CasClient::new("http://fake.cas");
+            let cas_with_auth = get_cas_client("auth/cas", CAS_URL, NoAuthBehavior::Authenticate);
+            let cas_with_403 =
+                get_cas_client("auth/cas", CAS_URL, NoAuthBehavior::AuthenticatedOr403);
+            let cas_with_404 =
+                get_cas_client("auth/cas", CAS_URL, NoAuthBehavior::AuthenticatedOr404);
             let cookie_store = CookieSession::signed(&[0; 32])
                 .secure(false)
                 .name(SESSION_COOKIE_NAME);
             App::new()
                 .wrap(cookie_store)
                 .wrap(middleware::Logger::default())
-                .data(cas.clone())
-                .service(guest)
-                .service(user)
-                .route("/login", web::get().to(urls::login))
-                .route("/logout", web::get().to(urls::logout))
+                .data(cas_with_auth.clone())
+                .route("/", web::get().to(guest))
+                .service(
+                    web::scope(USER_PATH)
+                        .wrap(cas_with_auth.clone())
+                        .route("", web::get().to(user)),
+                )
+                .service(
+                    web::scope(PROTECTED_PATH_403)
+                        .wrap(cas_with_403.clone())
+                        .route("", web::get().to(user)),
+                )
+                .service(
+                    web::scope(PROTECTED_PATH_404)
+                        .wrap(cas_with_404.clone())
+                        .route("", web::get().to(user)),
+                )
+                .route(LOGIN_PATH, web::get().to(urls::login))
+                .route(LOGOUT_PATH, web::get().to(urls::logout))
         });
         srv
+    }
+
+    #[actix_rt::test]
+    async fn test_redirect() {
+        // Check that URLs where the unauthorized behavior
+        // is to authorize produce a redirect.
+        let srv = get_server();
+        let req = srv.get(USER_PATH).send();
+        let resp = req.await.unwrap();
+        let headers = resp.headers();
+        let location_header = headers.get("location").unwrap();
+        let location_value = location_header.to_str().unwrap();
+        // TODO: Make this test better. The test merely checks
+        // for the presence of the CAS_URL inside of the location
+        // header. It would be better to have a more specific check.
+        assert!(location_value.contains(CAS_URL));
+    }
+
+    #[actix_rt::test]
+    async fn test_protected_403() {
+        let srv = get_server();
+        let req = srv.get(PROTECTED_PATH_403).send();
+        let resp = req.await.unwrap();
+        assert_eq!(resp.status(), StatusCode::FORBIDDEN);
+    }
+
+    #[actix_rt::test]
+    async fn test_protected_404() {
+        let srv = get_server();
+        let req = srv.get(PROTECTED_PATH_404).send();
+        let resp = req.await.unwrap();
+        assert_eq!(resp.status(), StatusCode::NOT_FOUND);
     }
 
     #[actix_rt::test]
     async fn test_cookie_is_set() {
         let srv = get_server();
         let req_1 = srv.get("/").send();
-        let resp_1 = req_1.await.unwrap();
-        let cookie_1 = resp_1
+        let resp = req_1.await.unwrap();
+        let cookie_1 = resp
             .cookies()
             .unwrap()
             .clone()
